b009acee367e472bd3628ec0eb31e95082140273
volodymyr.petryk <vladimir.petryk@88technologies.com>
2018-09-20 19:06:07 +0300

    Create project template

---
+++ src/main/java/com/asgardgames/ticktacktoe/TicktacktoeApplication.java
@@ -0,0 +1,12 @@
+package com.asgardgames.ticktacktoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicktacktoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicktacktoeApplication.class, args);
+	}
+}
---
+++ src/main/java/com/asgardgames/ticktacktoe/controllers/ChatController.java
@@ -0,0 +1,24 @@
+package com.asgardgames.ticktacktoe.controllers;
+
+import com.asgardgames.ticktacktoe.messagemodels.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMesage(@Payload Message message){
+        return message;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Message addUser(@Payload Message message, SimpMessageHeaderAccessor headerAccessor){
+        headerAccessor.getSessionAttributes().put("username", message.getSender());
+        return message;
+    }
+}
